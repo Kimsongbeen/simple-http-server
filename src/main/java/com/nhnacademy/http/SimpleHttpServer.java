76e5f3efef8a9d608146395a73e0f22aebd727da
Kimsongbeen <ksb10266@naver.com>
2024-10-02 17:12:49 +0900

    STEP3. simple-http-server

--- src/main/java/com/nhnacademy/http/SimpleHttpServer.java
+++ src/main/java/com/nhnacademy/http/SimpleHttpServer.java
@@ -1,21 +1,12 @@
-/*
- * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
- * + Copyright 2024. NHN Academy Corp. All rights reserved.
- * + * While every precaution has been taken in the preparation of this resource,  assumes no
- * + responsibility for errors or omissions, or for damages resulting from the use of the information
- * + contained herein
- * + No part of this resource may be reproduced, stored in a retrieval system, or transmitted, in any
- * + form or by any means, electronic, mechanical, photocopying, recording, or otherwise, without the
- * + prior written permission.
- * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
- */
-
 package com.nhnacademy.http;
 
 import lombok.extern.slf4j.Slf4j;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.atomic.AtomicLong;
+
 
 @Slf4j
 public class SimpleHttpServer {
@@ -23,30 +14,54 @@ public class SimpleHttpServer {
     private final int port;
     private static final int DEFAULT_PORT=8080;
 
+    private final AtomicLong atomicCounter;
+
     public SimpleHttpServer(){
         this(DEFAULT_PORT);
     }
-
     public SimpleHttpServer(int port) {
         if(port<=0){
-            throw new IllegalArgumentException(String.format("invalid port :%d",port));
+            throw new IllegalArgumentException(String.format("Invalid Port:%d",port));
         }
         this.port = port;
+        atomicCounter = new AtomicLong();
     }
 
-    public void start() throws IOException {
-        // #1 - SocketServer를 생성 합니다. PORT = 8080
-        try(ServerSocket serverSocket = new ServerSocket(DEFAULT_PORT)){
-            while(true){
-                // #2 - Client와 서버가 연결 되면 HttpRequestHandler를 이용해서 Thread을 생성 합니다.
-                Socket client = serverSocket.accept();
+    public void start(){
+        try(ServerSocket serverSocket = new ServerSocket(port);){
 
-                Thread thread = new Thread(new HttpRequestHandler(client));
-                thread.start();
+            HttpRequestHandler httpRequestHandlerA = new HttpRequestHandler();
+            HttpRequestHandler httpRequestHandlerB = new HttpRequestHandler();
+
+            // #9threadA를 생성하고 시작 합니다.
+            Thread threadA = new Thread(httpRequestHandlerA);
+            threadA.setName("ThreadA");
+            threadA.start();
+
+            // #10threadB를 생성하고 시작 합니다.
+            Thread threadB = new Thread(httpRequestHandlerB);
+            threadB.setName("ThreadB");
+            threadB.start();
+
+            while(true){
+                Socket client = serverSocket.accept();
+                /* #O11 count값이 짝수이면 httpRequestHandlerA에 client를 추가 합니다.
+                           count값이 홀수라면 httpRequestHandlerB에 clinet를 추가 합니다.
+                */
+                long count = atomicCounter.incrementAndGet();
+
+                log.debug("count: {}", atomicCounter);
+                if(count%2 == 0){
+                    httpRequestHandlerA.addRequest(client);
+                }else{
+                    httpRequestHandlerB.addRequest(client);
+                }
+
+                count++;
             }
-        }catch(Exception e){
-            log.debug("{}", e.getMessage());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
-
 }
